0b3a8bd283352a3d79c25ac2e351399dc33f9d15
pradhyankita <104745890+pradhyankita@users.noreply.github.com>
2022-07-11 12:18:55 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class Anagram {
 
         static void isAnagram(String str1, String str2) {
-            String s1 = str1.replaceAll("\\s", "");
-            String s2 = str2.replaceAll("\\s", "");
+            String s1 = str1.length();
+            String s2 = str2.length( );
             
         }
 
