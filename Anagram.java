592f702aa25dcb425cbb1f0e06204c1a7d875be8
pradhyankita <104745890+pradhyankita@users.noreply.github.com>
2022-07-12 08:08:01 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,31 +1,26 @@
 package com.BridgeLabz;
-import java.util.Arrays;
-public class Anagram {
-
-        static void isAnagram(String str1, String str2) {
-            String s1 = str1.replaceAll("\\s", "");
-            String s2 = str2.replaceAll("\\s", "");
-            boolean status = true;
-            if (s1.length() != s2.length()) {
-                status = false;
-            } else {
-                char[] ArrayS1 = s1.toLowerCase().toCharArray();
-                char[] ArrayS2 = s2.toLowerCase().toCharArray();
-                Arrays.sort(ArrayS1);
-                Arrays.sort(ArrayS2);
-                status = Arrays.equals(ArrayS1, ArrayS2);
-            }
-            if (status) {
-                System.out.println(s1 + " and " + s2 + " are anagrams");
-            } else {
-                System.out.println(s1 + " and " + s2 + " are not anagrams");
+import java.util.*;
+public class anagram{
+    public static void main(String []args){
+        String str1 = "SILEN";
+        String str2 = "LISTEN";
+        char c1[] = str1.toCharArray();
+        char c2[] = str2.toCharArray();
+        if(c1.length != c2.length)
+        {
+            System.out.println("Not Anagram");
+            System.exit(0);
+        }
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        for (int i = 0; i < c1.length; i++)
+        {
+            if (c1[i] != c2[i])
+            {
+                System.out.println("Not Anagram");
+                System.exit(0);
             }
         }
-
-        public static void main(String[] args) {
-            isAnagram("Keep", "Peek");
-            isAnagram("ab","ba");
-
-        }
+        System.out.println("Anagram");
+    }
 }
-
